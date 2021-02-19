f2e1e675e7ba7080b46c1ba97727eb5335335e8c
Andrew <a89506403090@gmail.com>
2021-02-19 23:44:27 +0500

    task01 chapter02 create object dog

---
+++ book_study_java/src/main/java/ru/littlecoder/chapter_02/dog/Dog.java
@@ -0,0 +1,14 @@
+package ru.littlecoder.chapter_02.dog;
+
+public class Dog {
+
+    int size;
+
+    String breed;
+
+    String name;
+
+    void bark() {
+        System.out.println("папа много ел караул холодмльник грабят");
+    }
+}
---
+++ book_study_java/src/main/java/ru/littlecoder/chapter_02/dog/DogTestDrive.java
@@ -0,0 +1,11 @@
+package ru.littlecoder.chapter_02.dog;
+
+public class DogTestDrive {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.size = 100;
+        dog.bark();
+    }
+    
+}
